855ac8c7b2bcff25cfc2b44aa26f9bcab7c1fe43
gdm-1819-woutvlae <woutvlae@student.arteveldehs.be>
2018-12-05 14:22:43 +0100

    Can't load images from src folder

--- src/pages/home.js
+++ src/pages/home.js
@@ -32,18 +32,27 @@ const logout = () => {
     .catch(error => console.log(error.message));
 };
 
+const populatePage = (id) => {
+  console.log(id);
+  db.ref(`users/${id}`).once('value')
+    .then((snapshot) => {
+      const data = snapshot.val();
+      console.log(data);
+      document.querySelector('#username').innerHTML = `${data.first_name} ${data.last_name}`;
+    });
+}
+
 export default () => {
   // Data to be passed to the template
   const logo = '../../src/assets/SVG/KotLife_Logo.svg';
   // Return the compiled template to the router
   // update(compile(homeTemplate)({ status, logo, user }));
   const status = firebase.auth().currentUser;
-  if(status) {
-    let user = db.ref(`users/${status.uid}`).once('value')
-      .then((snapshot) => {
-        let profile = snapshot.val()
-        update(compile(homeTemplate)({ logo, profile }));
-      });
+  if (status) {
+    update(compile(homeTemplate)({ logo, status }));
+    // Add handling of user data here
+    populatePage(status.uid);
+    document.querySelector('#logout-btn').addEventListener('click', logout);
   } else {
     update(compile(homeTemplate)({ logo }));
   }
