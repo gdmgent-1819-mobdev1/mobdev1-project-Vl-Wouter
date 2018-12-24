be9078a5a9a6b69bf72d988d66a6a22e12df4abe
Wouter Vlaeyen <36659001+Vl-Wouter@users.noreply.github.com>
2018-12-24 12:26:57 +0100

    Update: Working on messages

--- src/pages/conversation.js
+++ src/pages/conversation.js
@@ -7,3 +7,14 @@ import dataHelper from '../helpers/data-functions';
 const firebase = getInstance();
 
 const conversationTemplate = require('../templates/conversation.handlebars');
+
+export default () => {
+  // check for login
+  if(firebase.auth().currentUser) {
+    // get messages from conversation
+    // build text field for message
+    // when sent push message to conversation
+  } else {
+    window.location.replace('#/');
+  }
+}
